f784fd07b12c780f36cff5fcaecd661d5f01cfa3
RudyMontoo <rudraeng27@gmail.com>
2025-08-15 20:09:47 +0530

    ADD NOTES

--- src/TREES/LEC1/main.java
+++ src/TREES/LEC1/main.java
@@ -1,19 +1,19 @@
-package TREES.LEC1;
-
-public class main {
-    public static void main(String[] args) {
-//        Scanner scanner=new Scanner(System.in);
-//        BINARYTREE tree=new BINARYTREE();
-//        tree.populate(scanner);
-//        tree.prettyDisplay();
-int[] num={10,20,15,13,12};
-        BST2 tree1=new BST2();
-        System.out.println("Y");
-        tree1.min();
-        System.out.println("Y");
-        tree1.populate(num);
-        tree1.display();
-        System.out.println(tree1.balanced());
-        tree1.inorder();
-    }
-}
+//package TREES.LEC1;
+//
+//public class main {
+//    public static void main(String[] args) {
+////        Scanner scanner=new Scanner(System.in);
+////        BINARYTREE tree=new BINARYTREE();
+////        tree.populate(scanner);
+////        tree.prettyDisplay();
+//int[] num={10,20,15,13,12};
+//        BST2 tree1=new BST2();
+//        System.out.println("Y");
+//        tree1.min();
+//        System.out.println("Y");
+//        tree1.populate(num);
+//        tree1.display();
+//        System.out.println(tree1.balanced());
+//        tree1.inorder();
+//    }
+//}
--- src/sorting/cyclicsort.java
+++ src/sorting/cyclicsort.java
@@ -3,24 +3,20 @@ package sorting;
 import java.util.Scanner;
 
 public class cyclicsort {
-    static void Cyclic(int[] arr,int n){
-
-       int i=0;
-
-       while(i<n){
-           int correct=arr[i]-1;
-           if(i!=correct){
-               int temp=arr[i];
-               arr[i]=arr[correct];
-               arr[correct]=temp;
-           }
-           else{
-               i++;
-           }
-       }
-
-
+    static void Cyclic(int[] arr, int n) {
+        int i = 0;
+        while (i < n) {
+            int correct = arr[i] - 1;
+            // Only swap if the current element is not at its correct position
+            if (arr[i] != arr[correct]) {
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            } else {
+                i++;
+            }
         }
+    }
 
     public static void main(String[] args) {
         System.out.println("CYCLIC SORTING");
@@ -43,5 +39,4 @@ public class cyclicsort {
             System.out.print(" " + arr[i]);
         }
     }
-    }
-
+}
