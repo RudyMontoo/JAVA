e19356043fa3db50a77299f302fae2d791970cd2
Rudra Sharma <rudraeng27@gmail.com>
2026-08-11 08:42:52 +0530

    Time: 17 ms (41.56%), Space: 43.7 MB (74.44%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,10 +1,6 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+        if(s.length()!=t.length())return false;
         HashMap<Character, Character> mapST = new HashMap<>();
         HashMap<Character, Character> mapTS = new HashMap<>();
 
@@ -31,5 +27,6 @@ class Solution {
             }
         }
         return true;
+        
     }
 }
