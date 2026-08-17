c2c9e110d4d473e12acb39eb43b7e12c2c12a616
Rudra Sharma <rudraeng27@gmail.com>
2026-08-17 17:50:27 +0530

    Time: 0 ms (100%), Space: 46.2 MB (75.89%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -10,33 +10,51 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null)return null;
-        ListNode temp=head;
-        LinkedList<ListNode> odd=new LinkedList<>();
-         LinkedList<ListNode> even=new LinkedList<>();
-        int x=1;
-        while(temp!=null){
-            if(x==1){
-                odd.add(temp);
-                x=0;
-            }
-            else{
-                even.add(temp);
-                x=1;
-            }
-            temp=temp.next;
-        }
+        // METHOD 1 using collection framework
+    //     if(head==null)return null;
+    //     ListNode temp=head;
+    //     LinkedList<ListNode> odd=new LinkedList<>();
+    //      LinkedList<ListNode> even=new LinkedList<>();
+    //     int x=1;
+    //     while(temp!=null){
+    //         if(x==1){
+    //             odd.add(temp);
+    //             x=0;
+    //         }
+    //         else{
+    //             even.add(temp);
+    //             x=1;
+    //         }
+    //         temp=temp.next;
+    //     }
 
-        odd.addAll(even);
+    //     odd.addAll(even);
 
-          for (int i = 0; i < odd.size() - 1; i++) {
-        odd.get(i).next = odd.get(i + 1);
-    }
+    //       for (int i = 0; i < odd.size() - 1; i++) {
+    //     odd.get(i).next = odd.get(i + 1);
+    // }
 
    
-    odd.get(odd.size() - 1).next = null;
+    // odd.get(odd.size() - 1).next = null;
 
-    return odd.getFirst();
+    // return odd.getFirst();
+
+
+    // Method 2 OPTIMIZATION POINTER
+        if(head==null)return null;
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenhead=head.next;
+
+        while(even!=null && even.next!=null){
+            odd.next=odd.next.next;
+            odd=even.next;
+            even.next=even.next.next;
+            even=odd.next;
+
+        }
+        odd.next=evenhead;
+        return head;
 
     }
 }
