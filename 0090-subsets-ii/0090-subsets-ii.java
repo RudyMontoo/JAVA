e20038013891973661924caa17176b7e57d4f39f
Rudra Sharma <rudraeng27@gmail.com>
2026-08-18 18:30:32 +0530

    Time: 2 ms (99.71%), Space: 44.9 MB (77.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,34 +1,60 @@
+// // MY METHOD 
+
+// class Solution {
+//     final List<List<Integer>> result=new ArrayList<>();
+//     public List<List<Integer>> subsetsWithDup(int[] num) {
+//         Arrays.sort(num);
+//         func(0,num,new ArrayList<>());
+//         return result;
+//     }
+
+//      public  void func(int i, int[] num, List<Integer> list){
+//         if(i==num.length){
+//             if(check(list,result)){
+//             result.add(new ArrayList<>(list));
+//             }
+//         return; 
+//         }
+//         // take
+        
+//         list.add(num[i]);
+//         func(i+1,num,list);
+        
+
+        
+//         // not take 
+//        list.remove(list.size() - 1);
+//         func(i+1,num,list);
+//         return;
+
+
+//     }
+
+//     public boolean check(List<Integer> list, List<List<Integer>> result) {
+//     return !result.contains(list);
+// }
+// }
+
+
+// METHOD 2-> optimize method by recursive
 class Solution {
     final List<List<Integer>> result=new ArrayList<>();
-    public List<List<Integer>> subsetsWithDup(int[] num) {
+    public List<List<Integer>> subsetsWithDup(int[] num){
         Arrays.sort(num);
         func(0,num,new ArrayList<>());
         return result;
     }
+    public void func(int idx, int[] num, List<Integer> list){
+        result.add(new ArrayList<>(list));
+        // if(idx==num.length)return;
+        for(int i=idx;i<num.length;i++){
+            if(i!=idx && num[i]==num[i-1])continue;
 
-     public  void func(int i, int[] num, List<Integer> list){
-        if(i==num.length){
-            if(check(list,result)){
-            result.add(new ArrayList<>(list));
-            }
-        return; 
+            list.add(num[i]);
+            func(i+1,num,list);
+            list.remove(list.size()-1);
+
         }
-        // take
-        
-        list.add(num[i]);
-        func(i+1,num,list);
-        
-
-        
-        // not take 
-       list.remove(list.size() - 1);
-        func(i+1,num,list);
         return;
-
-
     }
-
-    public boolean check(List<Integer> list, List<List<Integer>> result) {
-    return !result.contains(list);
-}
-}
+    }
