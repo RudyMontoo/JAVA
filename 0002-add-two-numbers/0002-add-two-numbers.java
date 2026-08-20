2736d0a8fb7b5fb72e32e73591025851cbcea65e
Rudra Sharma <rudraeng27@gmail.com>
2026-08-20 18:12:26 +0530

    Time: 1 ms (100%), Space: 46.4 MB (77.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,72 +1,102 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+//         List<Integer> list1 = new ArrayList<>();
+//         List<Integer> list2 = new ArrayList<>();
+
+//         // Add values of l1
+//         while (l1 != null) {
+//             list1.add(l1.val);
+//             l1 = l1.next;
+//         }
+
+//         // Add values of l2
+//         while (l2 != null) {
+//             list2.add(l2.val);
+//             l2 = l2.next;
+//         }
+
+//         // Reverse both lists
+//         Collections.reverse(list1);
+//         Collections.reverse(list2);
+
+//         // Add digit by digit
+//         int i = list1.size() - 1;
+//         int j = list2.size() - 1;
+//         int carry = 0;
+
+//         List<Integer> result = new ArrayList<>();
+
+//         while (i >= 0 || j >= 0 || carry != 0) {
+
+//             int sum = carry;
+
+//             if (i >= 0) {
+//                 sum += list1.get(i);
+//                 i--;
+//             }
+
+//             if (j >= 0) {
+//                 sum += list2.get(j);
+//                 j--;
+//             }
+
+//             result.add(sum % 10);
+//             carry = sum / 10;
+//         }
+
+        
+        
+
+//         // Convert ArrayList<Integer> to ListNode
+//         ListNode dummy = new ListNode(0);
+//         ListNode temp = dummy;
+
+//         for (int num : result) {
+//             temp.next = new ListNode(num);
+//             temp = temp.next;
+//         }
+
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-
-        // Add values of l1
-        while (l1 != null) {
-            list1.add(l1.val);
-            l1 = l1.next;
-        }
-
-        // Add values of l2
-        while (l2 != null) {
-            list2.add(l2.val);
-            l2 = l2.next;
-        }
-
-        // Reverse both lists
-        Collections.reverse(list1);
-        Collections.reverse(list2);
-
-        // Add digit by digit
-        int i = list1.size() - 1;
-        int j = list2.size() - 1;
-        int carry = 0;
-
-        List<Integer> result = new ArrayList<>();
-
-        while (i >= 0 || j >= 0 || carry != 0) {
-
-            int sum = carry;
-
-            if (i >= 0) {
-                sum += list1.get(i);
-                i--;
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        int carry=0;
+       
+        while(l1!=null ||l2!=null || carry==1){
+             int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
             }
-
-            if (j >= 0) {
-                sum += list2.get(j);
-                j--;
+             if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
             }
-
-            result.add(sum % 10);
-            carry = sum / 10;
+            sum+=carry;
+            carry=sum/10;
+            ListNode node=new ListNode(sum%10);
+            
+            curr.next = node;
+            curr = curr.next;
         }
-
-        
-        
-
-        // Convert ArrayList<Integer> to ListNode
-        ListNode dummy = new ListNode(0);
-        ListNode temp = dummy;
-
-        for (int num : result) {
-            temp.next = new ListNode(num);
-            temp = temp.next;
-        }
-
         return dummy.next;
+            
+            
+        
     }
 }
