24c640bdc6b4bd09137e450d533b819278e0b081
Rudra Sharma <rudraeng27@gmail.com>
2026-08-21 12:59:05 +0530

    Time: 155 ms (28.67%), Space: 43.1 MB (28.29%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,21 +1,30 @@
 class Solution {
+
     public boolean exist(char[][] board, String word) {
-        StringBuffer sb = new StringBuffer();
+
         boolean[][] vis = new boolean[board.length][board[0].length];
+
         for (int i = 0; i < board.length; i++) {
-    for (int j = 0; j < board[0].length; j++) {
-        if (func(i, j, sb, board, word, vis)) {
-            return true;
+            for (int j = 0; j < board[0].length; j++) {
+
+                if (func(i, j, board, word, vis, 0)) {
+                    return true;
+                }
+            }
         }
-    }
-}
-return false;
-       
+
+        return false;
     }
 
-    public boolean func(int i, int j, StringBuffer sb,
-                        char[][] board, String word,
-                        boolean[][] vis) {
+    public boolean func(int i, int j,
+                        char[][] board,
+                        String word,
+                        boolean[][] vis,
+                        int index) {
+
+        if (index == word.length()) {
+            return true;
+        }
 
         if (i >= board.length || i < 0 ||
             j >= board[0].length || j < 0) {
@@ -26,25 +35,20 @@ return false;
             return false;
         }
 
-        if (board[i][j] != word.charAt(sb.length())) {
+        if (board[i][j] != word.charAt(index)) {
             return false;
         }
 
-        sb.append(board[i][j]);
         vis[i][j] = true;
 
-        if (sb.toString().equals(word)) {
+        if (func(i, j + 1, board, word, vis, index + 1) ||
+            func(i, j - 1, board, word, vis, index + 1) ||
+            func(i - 1, j, board, word, vis, index + 1) ||
+            func(i + 1, j, board, word, vis, index + 1)) {
+
             return true;
         }
 
-        if (func(i, j + 1, sb, board, word, vis) ||
-            func(i, j - 1, sb, board, word, vis) ||
-            func(i - 1, j, sb, board, word, vis) ||
-            func(i + 1, j, sb, board, word, vis)) {
-            return true;
-        }
-
-        sb.deleteCharAt(sb.length() - 1);
         vis[i][j] = false;
 
         return false;
