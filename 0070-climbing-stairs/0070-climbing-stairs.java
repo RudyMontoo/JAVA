9ec71b731b6f9fecc73fe05bdedaf9f2d5b62d4f
Rudra Sharma <rudraeng27@gmail.com>
2026-09-01 16:54:05 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,7 +8,16 @@ class Solution {
 
         // method 2 DP(memorization)
         int[] dp=new int[n+1];
-        return func(n,dp);
+        // return func(n,dp);
+
+        // method 3 DP(tabulation)
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
      
        
     }
@@ -23,12 +32,12 @@ class Solution {
     // }
 
 
-    public int func(int n, int[] dp){
-        if(n<0)return 0;
-        if(n==0)return 1;
-        if(dp[n]!=0)return dp[n];
-        dp[n]=func(n-1,dp)+func(n-2,dp);
-        return dp[n];
-    }
+    // public int func(int n, int[] dp){
+    //     if(n<0)return 0;
+    //     if(n==0)return 1;
+    //     if(dp[n]!=0)return dp[n];
+    //     dp[n]=func(n-1,dp)+func(n-2,dp);
+    //     return dp[n];
+    // }
 
 }
