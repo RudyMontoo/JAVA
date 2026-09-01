33216bd67aec98a060137c09e4a4969c7a0ac1b2
Rudra Sharma <rudraeng27@gmail.com>
2026-09-01 16:44:49 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.04%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,34 @@
+class Solution {
+   
+    public int climbStairs(int n) {
+        // method 1 DO USING RECURSION 
+      
+        // return func(n);
+
+
+        // method 2 DP(memorization)
+        int[] dp=new int[n+1];
+        return func(n,dp);
+     
+       
+    }
+    // below method is fail because its increase by 2^n
+    // public int func(int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     if(n<0)return 0;
+    //     return func(n-1)+func(n-2);
+        
+    // }
+
+
+    public int func(int n, int[] dp){
+        if(n<0)return 0;
+        if(n==0)return 1;
+        if(dp[n]!=0)return dp[n];
+        dp[n]=func(n-1,dp)+func(n-2,dp);
+        return dp[n];
+    }
+
+}
