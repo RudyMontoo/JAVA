52ea57d2b207e7142edbd02552bacad0d3b2de87
Rudra Sharma <rudraeng27@gmail.com>
2026-09-02 17:20:28 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,57 +1,24 @@
-// class Solution {
-//     int ans=0;
-//     public int rob(int[] nums){
-     
-//         int n=nums.length-1;
-//             func(n,0,nums);
-
-   
-//         return ans;
-//     }
-
-
-//     public void func(int idx, int sum, int[] nums){
-//         if(idx<0){
-//             ans=Math.max(ans,sum);
-//             return;
-//         }
-       
-        
-
-//         // pick
-//         func(idx-2,sum+nums[idx],nums);
-
-//         // not pick
-//         func(idx-1,sum,nums);
-
-        
-
-//     }
-// }
-
-
-import java.util.*;
-
 class Solution {
-    int[] dp;
-
     public int rob(int[] nums) {
-        dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-
-        return func(nums.length - 1, nums);
+        // firstly do with recursion
+        int n=nums.length;
+        // Arrays.fill(flag,0);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return func(n-1,nums,dp);
+        
+        
+    }
+    // METHOD 1  MEMORIZATION-> 67/70
+    public int func(int idx,int[]nums, int[] dp){
+        if(idx==0)return nums[idx];
+        if(idx<0)return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        int pick=nums[idx]+func(idx-2,nums,dp);
+        int notpick=0+func(idx-1,nums,dp);
+        dp[idx]=Math.max(pick, notpick);
+        return dp[idx];
     }
 
-    public int func(int idx, int[] nums) {
-        if (idx < 0)
-            return 0;
-
-        if (dp[idx] != -1)
-            return dp[idx];
-
-        int pick = nums[idx] + func(idx - 2, nums);
-        int notPick = func(idx - 1, nums);
-
-        return dp[idx] = Math.max(pick, notPick);
-    }
+    // 
 }
