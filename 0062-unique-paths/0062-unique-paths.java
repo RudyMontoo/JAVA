d62beb1bda371b481fe23158c8aac3ef490123aa
Rudra Sharma <rudraeng27@gmail.com>
2026-09-09 17:21:27 +0530

    Time: 1 ms (13.71%), Space: 42.4 MB (12.73%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,13 +6,30 @@ class Solution {
         // return ans;
 
         // METHOD 2 BY MEMOIZATION (dynamic programming)
-        int[][] dp=new int[m+1][n+1];
+        // int[][] dp=new int[m+1][n+1];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return func(m-1,n-1,dp);
+
+
+
+        // METHOD 3 By Tabulation (dynamic programming)
+        int[][] dp=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+                if(i==0 && j==0)dp[0][0]=1;
+                else{
+                    int up=i>0?dp[i-1][j]:0;
+                    int left=j>0?dp[i][j-1]:0;
+                    dp[i][j]=left+up;
+                }
             }
         }
-        return func(m-1,n-1,dp);
+        return dp[m-1][n-1];
+
 
     }
     // METHOD 1 BY RECURSION   (39/64)
@@ -39,18 +56,18 @@ class Solution {
 
 
      // METHOD 2 BY MEMOIZATION (dynamic programming)
-     private int func(int i, int j, int[][] dp){
-        if(i==0 && j==0){
-        return 1;
-        }
-        if(i<0 || j<0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+    //  private int func(int i, int j, int[][] dp){
+    //     if(i==0 && j==0){
+    //     return 1;
+    //     }
+    //     if(i<0 || j<0)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
 
-        int up=func(i-1,j,dp);
-        int left=func(i,j-1,dp);
-        dp[i][j]=left+up;
+    //     int up=func(i-1,j,dp);
+    //     int left=func(i,j-1,dp);
+    //     dp[i][j]=left+up;
         
-        return dp[i][j];
+    //     return dp[i][j];
         
-     }
+    //  }
 }
