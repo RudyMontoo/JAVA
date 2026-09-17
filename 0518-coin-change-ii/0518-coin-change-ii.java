41908c0bd8975381ac1747565eb92cd27da1b12b
Rudra Sharma <rudraeng27@gmail.com>
2026-09-17 14:52:49 +0530

    Time: 14 ms (41.87%), Space: 56.3 MB (56.89%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        // METHOD 1 RECURSION (TLE -> 14/31)
+        // METHOD 2 MEMOIZATION 
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return func(n-1, amount, coins, dp);
+    }
+
+    private static int func(int idx, int trg, int[] coins, int[][] dp){
+        if(trg==0 )return 1;
+        if(idx<0)return 0;
+        if(idx==0){
+            if(coins[idx]==trg)return 1;
+        }
+
+        if(dp[idx][trg]!=-1)return dp[idx][trg];
+        int take=0;
+        if(coins[idx]<=trg){
+            take=func(idx, trg-coins[idx], coins,dp);
+        }
+        int notTake=func(idx-1, trg, coins,dp);
+
+        return dp[idx][trg]=take+notTake;
+    }
+}
