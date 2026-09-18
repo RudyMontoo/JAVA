7d1a368c3588818670412258b6bf6ee74c2aecfa
Rudra Sharma <rudraeng27@gmail.com>
2026-09-18 09:27:20 +0530

    Time: 20 ms (77.87%), Space: 46.9 MB (69.86%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,4 +1,47 @@
 class Solution {
+    // METHOD 1
+//     public int minInsertions(String s) {
+//         int n = s.length();
+
+//         int[][] dp = new int[n][n];
+
+//         for (int i = 0; i < n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+
+//         return func(0, n - 1, s, dp);
+//     }
+
+//     public int func(int i, int j, String s, int[][] dp) {
+
+//         // Base case
+//         if (i >= j) {
+//             return 0;
+//         }
+
+//         // Already calculated
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+
+//         // Characters match
+//         if (s.charAt(i) == s.charAt(j)) {
+//             return dp[i][j] = func(i + 1, j - 1, s, dp);
+//         }
+
+//         // Characters don't match
+//         return dp[i][j] = 1 + Math.min(
+//             func(i + 1, j, s, dp),
+//             func(i, j - 1, s, dp)
+//         );
+//     }
+// }
+
+
+
+
+// METHOD 2
+
     public int minInsertions(String s) {
         int n = s.length();
 
@@ -8,30 +51,20 @@ class Solution {
             Arrays.fill(dp[i], -1);
         }
 
-        return func(0, n - 1, s, dp);
+        return n-func(0, n - 1, s, dp);
     }
 
     public int func(int i, int j, String s, int[][] dp) {
 
-        // Base case
-        if (i >= j) {
-            return 0;
+       if(i==j)return 1;
+        if(i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            dp[i][j]=2+func(i+1,j-1,s,dp);  
         }
-
-        // Already calculated
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Characters match
-        if (s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = func(i + 1, j - 1, s, dp);
-        }
-
-        // Characters don't match
-        return dp[i][j] = 1 + Math.min(
-            func(i + 1, j, s, dp),
-            func(i, j - 1, s, dp)
-        );
+        else dp[i][j]= Math.max(func(i+1,j,s,dp), func(i,j-1,s,dp));
+        return dp[i][j];
     }
+
+
 }
